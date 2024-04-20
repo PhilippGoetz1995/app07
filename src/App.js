bf43be6e2a79a1f13b3c44e6b4fa0dfd225615f2
Philipp Goetz <philipp.goetz@redbulls.com>
2024-04-20 22:16:12 +0200

    added edit functions to the button

--- src/App.js
+++ src/App.js
@@ -1,79 +1,89 @@
 import './index.css';
-import Employee from './components/Employee.js';
-
+import Employee from './components/Employee';
 import { useState } from 'react';
-
-//Importing uuid to have uniqe ids for emplyoees
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-
-  //Create a new state that can be changed
-  const [role, setRole] = useState('developer');
-
+  const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: 'Caleb',
       role: 'YouTube Sensation',
       img: 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg',
     },
     {
+      id: 2,
       name: 'Sal',
       role: 'Manager',
       img: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg',
     },
     {
+      id: 3,
       name: 'John',
       role: 'Director of Eng.',
       img: 'https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg',
     },
     {
+      id: 4,
       name: 'Melanie',
       role: 'Software Engineer',
       img: 'https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg',
     },
     {
+      id: 5,
       name: 'Corey',
       role: 'The Devops Guy',
       img: 'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg',
     },
     {
+      id: 6,
       name: 'Jake',
       role: 'Senior',
       img: 'https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg',
-    }
+    },
   ]);
 
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  const showEmployees = true;
   return (
-
     <div className="App">
-
-      <input
-        type="text"
-        onChange={(e) => {
-          console.log(e.target.value);
-          setRole(e.target.value);
-        }}
-        id="first"
-      />
-
-      <div className="flex flex-wrap justify-center">
-        {employees.map((employee) => {
-
-          console.log(uuidv4());
-
-          return (
-            <Employee
-              key={uuidv4()}
-              name={employee.name}
-              role={employee.role}
-              img={employee.img}
-            />
-          );
-        })}
-
-      </div>
-
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  updateEmployee={updateEmployee}
+                />
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
     </div>
   );
 }
