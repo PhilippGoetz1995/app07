2aa1df94e847e24cc8dd3572bc8cb841a708bb62
Philipp Goetz <philipp.goetz@redbulls.com>
2024-04-20 15:52:00 +0200

    Installing Tailwind

---
+++ src/components/Employee.js
@@ -0,0 +1,19 @@
+
+
+function Employee(props) {
+
+    return (
+    
+        <>
+            <h3> Taking the prop and display it: {props.name} </h3>
+
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+        </>
+        
+
+    )
+
+}
+
+export default Employee;
