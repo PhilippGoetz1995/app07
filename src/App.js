f362beeb2fae42c63bb18355d4e65030ebecdbbc
Philipp Goetz <philipp.goetz@redbulls.com>
2024-04-20 21:22:38 +0200

    Added bootstrap and first baby steps with tailwind.css

--- src/App.js
+++ src/App.js
@@ -3,31 +3,76 @@ import Employee from './components/Employee.js';
 
 import { useState } from 'react';
 
-function App() {
+//Importing uuid to have uniqe ids for emplyoees
+import { v4 as uuidv4 } from 'uuid';
 
-  const showEmployees = false;
+function App() {
 
   //Create a new state that can be changed
   const [role, setRole] = useState('developer');
+
+  const [employees, setEmployees] = useState([
+    {
+      name: 'Caleb',
+      role: 'YouTube Sensation',
+      img: 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg',
+    },
+    {
+      name: 'Sal',
+      role: 'Manager',
+      img: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg',
+    },
+    {
+      name: 'John',
+      role: 'Director of Eng.',
+      img: 'https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg',
+    },
+    {
+      name: 'Melanie',
+      role: 'Software Engineer',
+      img: 'https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg',
+    },
+    {
+      name: 'Corey',
+      role: 'The Devops Guy',
+      img: 'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg',
+    },
+    {
+      name: 'Jake',
+      role: 'Senior',
+      img: 'https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg',
+    }
+  ]);
 
   return (
 
     <div className="App">
 
-      <input 
-        type="text" 
-        onChange={(e) =>{
+      <input
+        type="text"
+        onChange={(e) => {
           console.log(e.target.value);
           setRole(e.target.value);
         }}
+        id="first"
       />
-       
-      {console.log('test')}
-      {showEmployees ? <p>show this</p> : <p>or show this</p>}
 
-        <Employee />
+      <div className="flex flex-wrap justify-center">
+        {employees.map((employee) => {
 
-        <Employee name={role} />
+          console.log(uuidv4());
+
+          return (
+            <Employee
+              key={uuidv4()}
+              name={employee.name}
+              role={employee.role}
+              img={employee.img}
+            />
+          );
+        })}
+
+      </div>
 
     </div>
   );
